ef83fb99d7ebc88b3d8fb999be55843b544bc0e5
Lekhu28 <87811795+Lekhu28@users.noreply.github.com>
2021-08-26 12:45:16 +0530

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+def frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+str = input("Enter a string: ")
+print(frequent(str))
